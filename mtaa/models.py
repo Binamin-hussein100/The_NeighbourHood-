21b59e663fb62fcf9475ec859406d8dcd4a791e0
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-28 12:49:42 +0300

    added user, neighbourhood and business models

--- mtaa/models.py
+++ mtaa/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models.expressions import F
 # Create your models here.
+
+class NeigbourHood(models.Model):
+    hood_name = models.CharField(max_length=30)
+    location = models.TextField()
+    occupants_count = models.IntegerField()
+    
+    def __str__(self):
+        return self.hood_name
+class Business(models.Model):
+    business_name = models.CharField(max_length=50)
+    neighbourhood = models.OneToOneField(NeigbourHood,unique=False,on_delete=models.CASCADE)
+    business_email = models.EmailField()
+
+class User(AbstractUser):
+    name = models.CharField(max_length=30)
+    neighborhood = models.OneToOneField(NeigbourHood,unique=False,on_delete=models.CASCADE)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.name
+    
