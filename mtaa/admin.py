982f54ad002c287e954bfb1cea2c8516bac34a15
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-09-29 09:27:36 +0300

    registerd the profile model

--- mtaa/admin.py
+++ mtaa/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User,NeigbourHood,Business,Profile
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(NeigbourHood)
+admin.site.register(Business),
+admin.site.register(Profile),
